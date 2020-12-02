489e982166c4e9a8badd71261df00ed1f43d70ff
Calum Leask <calum.leask@wrld3d.com>
2020-12-02 10:22:58 +0000

    Installing and using wrld-react (#1)
    * Installing and using wrld-react
    * Removing dependency on wrld.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,37 @@
-/* App.js */
+/* globals WrldIndoorControl */
 
-import WrldMap from "./WrldMap";
+import { WrldMap } from "wrld-react";
 
-function App() {
-  
+const App = () => {
   return (
-    <WrldMap/>
+    <div>
+      <WrldMap
+        apiKey={"your_api_key_here"}
+        containerStyle={{
+          width: "600px",
+          height: "400px"
+        }}
+        mapOptions={{
+          center: [56.459604, -2.977816],
+          indoorsEnabled: true
+        }}
+        onMapMount={(map) => {
+          new WrldIndoorControl("wrld-indoor-control", map);
+        }}
+      >
+        <div
+          id={"wrld-indoor-control"}
+          style={{
+            position: "absolute",
+            top: "20px",
+            right: "20px",
+            bottom: "40px"
+          }}
+        >
+        </div>
+      </WrldMap>
+    </div>
   );
-}
+};
 
 export default App;
